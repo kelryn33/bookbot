0a56879f3057df99b6479bc41699e3e0a7c18bac
kelryn33 <liz.zielke@gmail.com>
2024-03-22 14:03:55 -0500

    Redid Main.py a second time to enforce lessons

--- main.py
+++ main.py
@@ -1,54 +1,58 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    chars_dict = get_chars_dict(text)
-    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    word_count = get_word_count(text)
+    letter_count = get_letter_count(text)
+    sorted_letter_count = get_sorted_letter_count(letter_count)
 
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{num_words} words found in the document")
+    print()
+    print(f"----Begin Report of Document:{book_path}----")
+    print()
+    print(f"Found {word_count} words in the document")
+    print()
+    print(f"Here is a breakdown of the letters used in the document:")
+    print()
+    
+    for letter in sorted_letter_count:
+        if not letter['letter'].isalpha():
+            continue
+        print(f"Letter '{letter['letter']}' appears: {letter['num']} times")
+
+    print()
+    print(f"----End of Report on Document:{book_path}----")
     print()
 
-    for item in chars_sorted_list:
-        if not item["char"].isalpha():
-            continue
-        print(f"The '{item['char']}' character was found {item['num']} times")
+def sort_on(dict):
+    return dict["num"]
 
-    print("--- End report ---")
+def get_sorted_letter_count(dict):
+    sorted_letters = []
+    for i in dict:
+        sorted_letters.append({"letter": i, "num": dict[i]})
+        sorted_letters.sort(reverse=True, key=sort_on)
+    return sorted_letters
+  
 
-
-def get_num_words(text):
+def get_word_count(text):
     words = text.split()
     return len(words)
 
-
-def sort_on(d):
-    return d["num"]
-
-
-def chars_dict_to_sorted_list(num_chars_dict):
-    sorted_list = []
-    for ch in num_chars_dict:
-        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
-    sorted_list.sort(reverse=True, key=sort_on)
-    return sorted_list
-
-
-def get_chars_dict(text):
-    chars = {}
-    for c in text:
-        lowered = c.lower()
-        if lowered in chars:
-            chars[lowered] += 1
+def get_letter_count(text):
+    letters = {}
+    for i in text:
+        lower_letter = i.lower()
+        if lower_letter in letters:
+            letters[lower_letter] += 1
         else:
-            chars[lowered] = 1
-    return chars
+            letters[lower_letter] = 1
+    return letters
+        
 
 
-def get_book_text(path):
-    with open(path) as f:
+def get_book_text(text):
+    with open(text) as f:
         return f.read()
 
 
-main()
 
+main()
